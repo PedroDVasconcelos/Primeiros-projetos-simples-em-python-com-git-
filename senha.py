8436141e1b0c75a1f8efe4a9527b317e163ac8bc
Pedro D. <pedrohenriquediasvas@gmail.com>
2025-11-27 09:24:15 -0300

    Add files via upload

---
+++ senha.py
@@ -0,0 +1,9 @@
+
+senha = input("Digite a senha:\n")
+verifi_senha = input("Digite a senha novamente\n")
+if (senha == verifi_senha):
+    print("\nBem vindo!")
+else:
+    print("Senha incorreta,tente novamente")
+
+
